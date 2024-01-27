b67abea93425ce3bbf423e4eeb30257f0d1bd4ef
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-01-27 18:20:32 +0300

    Task done

---
+++ src/Entity/Employee.java
@@ -0,0 +1,60 @@
+package Entity;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import Entity.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Employee> employes = new LinkedList<>();
+        employes.add(new Employee(1,"yasin","ermusmula"));
+        employes.add(new Employee(1,"yasin","ermusmula"));
+        employes.add(new Employee(2,"ali","veli"));
+        employes.add(new Employee(2,"ahmet","mehmet"));
+        employes.add(new Employee(3,"hasan","huseyin"));
+        employes.add(new Employee(3,"mahmut","tarkan"));
+        employes.add(new Employee(4,"hande","er"));
+        employes.add(new Employee(4,"sevim","dert"));
+
+        List<Employee> removedEmploye = new ArrayList<>();
+        Map<Integer,Employee> employeeMap = new HashMap<>();
+
+        Iterator<Employee> iterator = employes.iterator();
+        while (iterator.hasNext()){
+            Employee employee = iterator.next();
+            if (employee == null){
+                System.out.println("null data buldum");
+                continue;
+            }
+            if (employeeMap.containsKey(employee.getId())){
+                removedEmploye.add(employee);
+                iterator.remove();
+            }
+            else {
+                employeeMap.put(employee.getId(), employee);
+            }
+        }
+
+        System.out.println("mevcut:");
+        System.out.println(employes);
+        System.out.println("tekiller: ");
+        System.out.println(employeeMap);
+        System.out.println("silinenler: ");
+        System.out.println(removedEmploye);
+
     }
 }
